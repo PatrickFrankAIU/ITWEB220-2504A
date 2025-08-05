50c40d16fe10e9a2ec049ccfb7cb37ea03621164
Pat Frank <pfrank@aiufl.edu>
2025-08-05 16:13:02 -0400

    instructor comment

--- Mod1Exr/main.js
+++ Mod1Exr/main.js
@@ -1,5 +1,5 @@
 let userName = prompt("Please enter your name:");
 let var1 = prompt("Please enter a number between 1 and 10:");
 let var2 = prompt("Please enter a number between 11 and 20:");
-let userResult = var1 * var2
+let userResult = var1 * var2  // Just need a semi-colon here! -- Pat
 console.log("Thank you, " + userName + ". The result of " + var1 + " x " + var2 + " is: " + userResult);
